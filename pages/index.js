80802bc1211f0eb05a4919cedf72913e1b987f73
coderlore <coderlore@gmail.com>
2023-11-19 13:34:16 -1000

    Added images; cleanup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
+import { getFeaturedEvents } from "../dummy-data";
+
 function HomePage() {
+    const featuredEvents = getFeaturedEvents
+
     return (
         <div>
             <h1>HomePage!</h1>
+            
         </div>
     )
 }
